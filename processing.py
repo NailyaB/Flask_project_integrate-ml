05fd8717798e0e66c69f4ec90e441309a5d3bc52
nailya <nailya@gmail.com>
2024-11-09 20:55:00 +0300

    обновлен документ processing.py

--- processing.py
+++ processing.py
@@ -3,12 +3,12 @@ import pickle
 import numpy as np
 
 def process(scaled_coef):
-    with open("E:/andan/Flask_project_integrate-ml/models/lr_model.pkl", "rb") as f:
+    with open("./models/lr_model.pkl", "rb") as f:
         regressor = pickle.load(f)
     predicted_mu = regressor.predict(scaled_coef) 
     
 
-    with open("E:/andan/Flask_project_integrate-ml/models/scaler_y.pkl", "rb") as f:
+    with open("./models/scaler_y.pkl", "rb") as f:
         min_max_scaler_y = pickle.load(f)
     mu = np.round(min_max_scaler_y.inverse_transform(predicted_mu.reshape(1, -1)), 2)
         
@@ -16,7 +16,7 @@ def process(scaled_coef):
 
 
 def preprocess(coef):
-    with open("E:/andan/Flask_project_integrate-ml/models/scaler_x.pkl", "rb") as f:
+    with open("./models/scaler_x.pkl", "rb") as f:
         min_max_scaler_x = pickle.load(f)
 
     scaled_coef = min_max_scaler_x.transform ([[coef]])
